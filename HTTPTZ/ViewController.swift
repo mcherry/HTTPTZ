618194385d9382d320a62358e0ad1168e0226219
Michael Cherry <mcherry@sps186.org>
2020-07-31 16:54:25 -0500

    Initial Commit

---
+++ HTTPTZ/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HTTPTZ
+//
+//  Created by Mike Cherry on 7/31/20.
+//  Copyright © 2020 Mike Cherry. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
